0896a364852f50d83193489b5d8fd1da5422fc1c
iamanish1 <animetra14@gmail.com>
2025-03-30 17:13:16 +0530

    update  in code

--- client/src/pages/LoginAccountPage.jsx
+++ client/src/pages/LoginAccountPage.jsx
@@ -4,6 +4,7 @@ import SingupPage from "../components/SignupPage";
 const LoginAccountPage = ()=>{
     return (
         <>
+        {/* This is my page  */}
         <SingupPage/>
         </>
     )
