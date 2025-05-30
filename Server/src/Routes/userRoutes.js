6102331c1d04c0edf94a3e8de94deeb319f827ad
iamanish1 <animetra14@gmail.com>
2025-05-30 18:08:58 +0530

    done with the update profile also

--- Server/src/Routes/userRoutes.js
+++ Server/src/Routes/userRoutes.js
@@ -4,7 +4,8 @@ import { LoginUser } from '../controller/LoginUserController.js';
 import githubAuthentication from '../controller/githubauthController.js';
 import { GetRegisterUser } from '../controller/GetUserDetailController.js';
 import authMiddleware from '../Middleware/authenticateMiddelware.js';
-import {editUserProfile, getUserProfile} from '../controller/EditProfileController.js';
+import {editUserProfile, getUserProfile , updateProfile} from '../controller/EditProfileController.js';
+
 const userRoute = express.Router() ; 
 
 userRoute.post('/user', authenticateUser); 
@@ -13,5 +14,6 @@ userRoute.post("/github/login", githubAuthentication) ;
 userRoute.get("/getuser",authMiddleware,GetRegisterUser) ;
 userRoute.post("/editprofile", authMiddleware,editUserProfile) ; 
 userRoute.get("/profile", authMiddleware, getUserProfile) ; 
+userRoute.put("/updteprofile", authMiddleware, updateProfile ) ;
 
 export default userRoute ; 
