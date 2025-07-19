210c88bec04deafeb91e8617da43bcbb78f47fb4
anish <animetra14@gmail.com>
2025-07-19 17:12:41 +0530

    update in code

--- Server/src/Routes/ChatRoutes.js
+++ Server/src/Routes/ChatRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { getProjectMessages } from "../controller/ChatController.js";
 
 const chatRoutes = express.Router();
-chatRoutes.get("/chat", getProjectMessages);
+chatRoutes.get("/chat/:projectId", getProjectMessages);
 
 export default chatRoutes;
